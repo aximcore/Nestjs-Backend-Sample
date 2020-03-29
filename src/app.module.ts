e7e5c8bcbfa84f74646a8f8f2fa1970a2cc8556f
Máté Tóth <aximcore@gmail.com>
2020-03-29 14:10:51 +0200

    feat: added typeorm and mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+/**
+ * mysql 8 strong password auth won't supported
+ * mysql: ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+ * mysql: flush privileges;
+ */
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule, 
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
